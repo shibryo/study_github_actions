059bbcdc3dd9bf368c31061828932770c6ce814c
r_shibata <ryosuke9913@yahoo.co.jp>
2022-07-30 20:43:30 +0900

    Update test_prime.py

--- tests/test_prime.py
+++ tests/test_prime.py
@@ -4,7 +4,7 @@ from poetry_project.prime import is_prime
 
 
 def test_is_prime():
-    assert not is_prime(1)
+    assert is_prime(1)
     assert is_prime(2)
     assert is_prime(3)
     assert not is_prime(4)
